11b26088f7e3f23b5a43039776f3c2326f1b6de0
marlonrmv85 <123046046@upq.edu.mx>
2026-05-12 18:37:33 -0600

    practica_1

---
+++ Practica1/logica.js
@@ -0,0 +1,3 @@
+let nombre=prompt("cuales es tu nombre: ");
+        console.log("Hola: "+ nombre +"!")
+        document.write("<h1>Hola,: "+ nombre +"!</h1>")
---
+++ Practica1/servidor.js
@@ -0,0 +1,24 @@
+
+console.log("Hola mundo JS desde el servidor")  
+
+
+
+//promedio 2 variables 
+let edad1=11
+let edad2=33
+console.log("edad promedio")
+console.log((edad1+edad2)/2)
+
+//medir el timpo de procesos 
+
+console.time("mi proceso")
+    for(let i=0; i<1000000; i++){}
+console.timeEnd("miProceso")
+
+
+// objetos tipos tabla
+let usuarios=[
+    {nombre:"marlon",edad:"21"},
+    {nombre:"mendieta",edad:"21"},
+]
+console.table(usuarios)
